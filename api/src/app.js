ad9b29be6e0131569991929d09a54eabbb57e020
Gustavo Larsen <55494775+gustavolarsen@users.noreply.github.com>
2020-03-28 15:38:53 -0300

    Adiconando as rotas

--- api/src/app.js
+++ api/src/app.js
@@ -3,13 +3,13 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    const { nome, idade } = req.query;
-    return res.send({ message: `Eu sou ${nome} e tenho ${idade} anos.` });
-});
+const indexRoute = require('./Routes/index');
+const usersRoute = require('./Routes/users');
 
+app.use('/', indexRoute),
+    app.use('/users', usersRoute);
 
-app.listen('3333');
+app.listen('3000');
 
 module.exports = app;
 
