722d71c877c78f13a6b5cadf0a6439cc1051cd1a
Gustavo Larsen <guslarsen@gmail.com>
2020-03-29 21:38:45 -0300

    Update config.js

--- api/config/config.js
+++ api/config/config.js
@@ -2,7 +2,8 @@
 
 const config = () => {
     return {
-        connectionString: 'mongodb+srv://guslarsen:guslarsen2020@cluster0-zno9q.mongodb.net/desappegdb',
+        //para fins de segurança o usuario e senha da conexão foram removidos
+        connectionString: 'mongodb+srv://<user>:<password>@cluster0-zno9q.mongodb.net/usersdb',
         jtwKey: 'd41d8cd98f00b204e9800998ecf8427e',
         jwtExpiresIn: '3d'
     }
